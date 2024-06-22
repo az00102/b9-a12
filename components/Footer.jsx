52b1d91f8b51887e86700debee24efa33b9ca798
az00102 <nayamulazim1899@gmail.com>
2024-06-22 21:39:16 +0600

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,86 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div className="bg-white dark:bg-gray-800 text-black dark:text-white">
+            <footer className="px-4 divide-y static w-full">
+                <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+                    <div className="lg:w-1/3">
+                        <div className='flex '>
+                            <img className='w-12 ml-2 rounded-full border border-[#6CBF40]' src="/favicon.webp" alt="" />
+                            <Link to={'/'} className="btn btn-ghost text-lg lg:text-3xl font-bold">AlternativeZone</Link>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
+                        <div className="space-y-3">
+                            <h3 className="tracking-wide uppercase ">Product</h3>
+                            <ul className="space-y-1">
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Features</a>
+                                </li>
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Integrations</a>
+                                </li>
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Pricing</a>
+                                </li>
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">FAQ</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="space-y-3">
+                            <h3 className="tracking-wide uppercase ">Company</h3>
+                            <ul className="space-y-1">
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Privacy</a>
+                                </li>
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Terms of Service</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="space-y-3">
+                            <h3 className="uppercase ">Developers</h3>
+                            <ul className="space-y-1">
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Public API</a>
+                                </li>
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Documentation</a>
+                                </li>
+                                <li>
+                                    <a rel="noopener noreferrer" href="#">Guides</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="space-y-3">
+                            <div className="uppercase ">Social media</div>
+                            <div className="flex justify-start space-x-3">
+                                <a rel="noopener noreferrer" href="https://www.facebook.com/" title="Facebook" className="flex items-center p-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+                                        <path d="M32 16c0-8.839-7.167-16-16-16-8.839 0-16 7.161-16 16 0 7.984 5.849 14.604 13.5 15.803v-11.177h-4.063v-4.625h4.063v-3.527c0-4.009 2.385-6.223 6.041-6.223 1.751 0 3.584 0.312 3.584 0.312v3.937h-2.021c-1.984 0-2.604 1.235-2.604 2.5v3h4.437l-0.713 4.625h-3.724v11.177c7.645-1.199 13.5-7.819 13.5-15.803z"></path>
+                                    </svg>
+                                </a>
+                                <a rel="noopener noreferrer" href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_nav-header-signin" title="LinkedIn" className="flex items-center p-1">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        width="24"
+                                        height="24"
+                                        fill="currentColor"
+                                    >
+                                        <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.025-3.064-1.869-3.064-1.871 0-2.159 1.462-2.159 2.972v5.696h-3v-10h2.875v1.377h.041c.4-.759 1.379-1.561 2.839-1.561 3.036 0 3.596 2.001 3.596 4.6v5.584z" />
+                                        </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="py-6 text-sm text-center ">© 1968 Company Co. All rights reserved.</div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
